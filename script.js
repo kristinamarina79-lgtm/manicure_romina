d911da50c62e2dc0435dbae112a31d99ce59b2b4
kristinamarina79-lgtm <kristinamarina79@gmail.com>
2026-02-10 13:43:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Nail website loaded");
+document.querySelector(".booking-form")?.addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you! I will contact you soon 💅");
+});
